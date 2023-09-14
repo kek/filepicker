eed02ea9a4b86365e15089eb6476513d1f636532
Karl Eklund <localpart@gmail.com>
2023-09-14 18:41:09 +0200

    Rename model to FilePickerModel

--- main.go
+++ main.go
@@ -7,29 +7,29 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
+type FilePickerModel struct {
 	gizmos   int
 	gremlins int
 }
 
-func initialModel() model {
-	return model{gizmos: 1, gremlins: 0}
+func NewModel() FilePickerModel {
+	return FilePickerModel{gizmos: 1, gremlins: 0}
 }
 
-func (m model) Init() tea.Cmd {
+func (m FilePickerModel) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(tea.Msg) (tea.Model, tea.Cmd) {
+func (m FilePickerModel) Update(tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) View() string {
+func (m FilePickerModel) View() string {
 	return "hello world"
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(NewModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
