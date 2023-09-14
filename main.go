89b6cc8fce08d239bd99d8d41acf9ab02ea57e33
Karl Eklund <localpart@gmail.com>
2023-09-14 18:37:29 +0200

    Implement tea.Model interface for model

--- main.go
+++ main.go
@@ -16,6 +16,18 @@ func initialModel() model {
 	return model{gizmos: 1, gremlins: 0}
 }
 
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return "hello world"
+}
+
 func main() {
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
